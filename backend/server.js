e8af8d3259fc2644d90b0781595a18492d5a8f39
Swag-blips <coderblip@gmail.com>
2024-09-26 23:54:09 +0100

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 6000;
 app.get("/", (req, res) => {
   res.send("server is ready");
 });
-app.listen(5000, () => {
-  console.log("Server is running on 5000");
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
