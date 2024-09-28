9ee6802c2b482bb72268abf5871400fbf8e12648
Swag-blips <coderblip@gmail.com>
2024-09-28 10:09:09 +0100

    backend complete

---
+++ backend/controller/user.controller.js
@@ -0,0 +1,16 @@
+import User from "../model/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+
+    const filteredusers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    return res.status(200).json(filteredusers);
+  } catch (error) {
+    console.log(`error in getUsersForSidebar ${error.message}`);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
 import connectMongoDb from "./db/connectMongoDb.js";
 import cookieParser from "cookie-parser";
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
   connectMongoDb();
