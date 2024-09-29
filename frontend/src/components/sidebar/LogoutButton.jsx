be3471986aa4b424f3290f9bf9e67cecec29a07e
Swag-blips <coderblip@gmail.com>
2024-09-29 12:21:17 +0100

    feat:messages design

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BiLogOut } from "react-icons/bi";
+
+const LogoutButton = () => {
+  return (
+    <di className="mt-auto">
+      <BiLogOut className="w-6 h-6 text-white cursor-pointer" />
+    </di>
+  );
+};
+
+export default LogoutButton;
