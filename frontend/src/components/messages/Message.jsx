6e8982c65a6f594410779f4cd41ad9bd3e87e0aa
Swag-blips <coderblip@gmail.com>
2024-10-02 14:25:11 +0100

    application complete

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -13,6 +13,7 @@ const Message = ({ message }) => {
     ? authUser.profilePic
     : selectedConversation?.profilePic;
   const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+  const shakeClass = message.shouldShake ? "shake" : "";
   return (
     <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
@@ -21,7 +22,9 @@ const Message = ({ message }) => {
         </div>
       </div>
 
-      <div className={`chat-bubble text-white  ${bubbleBgColor} pb-2`}>
+      <div
+        className={`chat-bubble text-white  ${bubbleBgColor} ${shakeClass} pb-2`}
+      >
         {message.message}
       </div>
 
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useRef } from "react";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 import useGetMessages from "../../hooks/useGetMessages";
 import Message from "./Message";
+import useListenMessages from "../../hooks/useListenMEssages";
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
+  useListenMessages();
   const lastMessageRef = useRef();
 
   useEffect(() => {
